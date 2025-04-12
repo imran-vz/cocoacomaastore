06a393ce7ceadff4b1988faf07fb23356f75eeba
Mohammed Imran <mohammedimran86992@gmail.com>
2025-04-12 16:01:40 +0530

    Add revalidation setting to admin page: Introduce a revalidate constant to control data fetching behavior for improved performance.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,8 @@
 import { getDesserts } from "./actions";
 import ManageDesserts from "./manage-desserts";
 
+export const revalidate = 0;
+
 export default async function page() {
 	const desserts = await getDesserts();
 
