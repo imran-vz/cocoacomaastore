d739c4b398b272f65112c539d0058aa2e4135179
Mohammed Imran <mohammedimran86992@gmail.com>
2025-04-14 11:26:12 +0530

    Refactor DessertList component: Update card layout and styling for dessert items, enhancing readability and visual hierarchy. Change heading and price display to improve user experience.

--- src/components/dessert-list.tsx
+++ src/components/dessert-list.tsx
@@ -21,10 +21,14 @@ export function DessertList({ desserts, addToCart }: DessertListProps) {
 				<Card key={dessert.id} className="w-full shadow-none py-4 gap-2">
 					<CardHeader>
 						<div className="flex justify-between items-center">
-							<h3 className="font-semibold text-base min-w-32 max-w-32 capitalize">
-								{dessert.name}
-							</h3>
-							<p className="font-bold text-base">{dessert.price.toFixed(2)}</p>
+							<div className="flex-1">
+								<h4 className="font-medium text-sm capitalize">
+									{dessert.name}
+								</h4>
+								<p className="text-xs text-muted-foreground">
+									{dessert.price.toFixed(2)}
+								</p>
+							</div>
 							<Button onClick={() => addToCart(dessert)} size="sm">
 								Add to Cart
 							</Button>
