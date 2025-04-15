9724a94708b511b84cb2a6585c25b6daf3912ab2
Mohammed Imran <mohammedimran86992@gmail.com>
2025-04-15 11:34:34 +0530

    make the card clickable and change the layout to grid with 2 columns

--- src/components/dessert-list.tsx
+++ src/components/dessert-list.tsx
@@ -11,25 +11,24 @@ interface DessertListProps {
 
 export function DessertList({ desserts, addToCart }: DessertListProps) {
 	return (
-		<div className="flex flex-col gap-2">
+		<div className="grid grid-cols-2">
 			{desserts.map((dessert) => (
-				<Card key={dessert.id} className="w-full shadow-none py-3 gap-2">
-					<CardContent className="px-4">
-						<div className="flex justify-between items-center">
-							<div className="flex-1">
-								<h4 className="font-bold text-base capitalize">
-									{dessert.name}
-								</h4>
-								<p className="text-xs text-muted-foreground">
-									{dessert.price.toFixed(2)}
-								</p>
+				<Button asChild key={dessert.id} onClick={()=>addToCart(dessert)}>
+					<Card  className="w-full shadow-none py-3 gap-2">
+						<CardContent className="px-4">
+							<div className="flex justify-between items-center">
+								<div className="flex-1">
+									<h4 className="font-bold text-base capitalize">
+										{dessert.name}
+									</h4>
+									<p className="text-xs text-muted-foreground">
+										{dessert.price.toFixed(2)}
+									</p>
+								</div>
 							</div>
-							<Button onClick={() => addToCart(dessert)} size="sm">
-								Add to Cart
-							</Button>
-						</div>
-					</CardContent>
-				</Card>
+						</CardContent>
+					</Card>
+				<Button>
 			))}
 		</div>
 	);
