1c760e0a1ceb30f9569e7b71ca520ad8cdadc904
Mohammed Imran <mohammedimran86992@gmail.com>
2025-12-16 19:15:16 +0530

    Update dependencies and refactor database connection: Upgraded @types/node to 24.10.4, removed ws dependency from neonConfig, and adjusted package.json scripts for improved compatibility with bun.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,5 @@
-import { neonConfig, Pool } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
-import ws from "ws";
 
 import * as schema from "./schema";
 
@@ -8,7 +7,6 @@ if (!process.env.DATABASE_URL) {
 	throw new Error("DATABASE_URL is not set");
 }
 
-neonConfig.webSocketConstructor = ws;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 export const db = drizzle({
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,15 +1,13 @@
 import "dotenv/config";
 
-import { neonConfig, Pool } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
-import ws from "ws";
 import { dessertsTable, orderItemsTable, ordersTable } from "./schema";
 
 if (!process.env.DATABASE_URL) {
 	throw new Error("DATABASE_URL is not set");
 }
 
-neonConfig.webSocketConstructor = ws;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 const db = drizzle({
